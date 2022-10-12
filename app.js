200b6488b2ed5ee993e22847892f8ea4c6b2d112
MurtazaNeher <96943172+MurtazaNeher@users.noreply.github.com>
2022-10-12 16:34:49 +0530

    Update app.js

--- app.js
+++ app.js
@@ -14,7 +14,6 @@ function checkProfitAndLoss() {
     let qty = Number(quantity.value);
     let cp = Number(currentPrice.value);
 
-    console.log(ip,qty,cp);
     if (ip>0 && qty>0 && cp>0) {
 
         if (cp > ip) {
